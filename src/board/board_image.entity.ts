378c44e31c3df290b2ae1ea6724a96596af8f45c
EasternPen9uin <yck723@naver.com>
2023-05-13 20:44:24 +0900

    Added some entity files.

---
+++ src/board/board_image.entity.ts
@@ -0,0 +1,15 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, ManyToOne, Column } from "typeorm";
+import { BoardTable } from "./board_table.entity";
+
+@Entity()
+export class BoardImage extends BaseEntity
+{
+    @PrimaryGeneratedColumn()
+    board_image_id : number;
+
+    @ManyToOne(type=>BoardTable, board_table => board_table.board_images, {eager : false})
+    board : BoardTable;
+
+    @Column({type : 'character varying'})
+    board_image : string;
+}
