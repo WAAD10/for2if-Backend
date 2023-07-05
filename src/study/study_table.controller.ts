f071d9a2018c9cd57a3802bf83b3170fde0c5225
PH5555 <lmkn5342@gmail.com>
2023-07-05 16:30:38 +0900

    study input

--- src/study/study_table.controller.ts
+++ src/study/study_table.controller.ts
@@ -1,23 +1,37 @@
-import { Controller, Get } from "@nestjs/common";
-import { StudyTable } from "./study_table.entity";
-import { StudyTableRepository } from "./study_table.repository";
-
-
+import { Controller, Get, Post } from '@nestjs/common';
+import { StudyTable } from './study_table.entity';
+import { StudyTableRepository } from './study_table.repository';
+import { StudyDto } from './dto/study.dto';
 
 @Controller('study')
 export class StudyTableController {
-    constructor(
-        private studyTableRepository : StudyTableRepository
-    ) {}
+  constructor(private studyTableRepository: StudyTableRepository) {}
 
+  @Post('/')
+  async createStudy(studyDto: StudyDto) {
+    await this.studyTableRepository
+      .createQueryBuilder()
+      .insert()
+      .into(StudyTable)
+      .values([
+        {
+          study_date: studyDto.study_date,
+          study_name: studyDto.study_name,
+          study_image: studyDto.study_image,
+        },
+      ])
+      .execute();
 
-    @Get('/')
-    async getAllStudy(
-    ) : Promise<{study:StudyTable[]}> {
-        const query = this.studyTableRepository.createQueryBuilder('study_table');
-        const study_list : StudyTable[] = await query.getMany();
-        const ret = {study : study_list};
-        return ret;
-    }
+    return {
+      success: 'true',
+    };
+  }
 
+  @Get('/')
+  async getAllStudy(): Promise<{ study: StudyTable[] }> {
+    const query = this.studyTableRepository.createQueryBuilder('study_table');
+    const study_list: StudyTable[] = await query.getMany();
+    const ret = { study: study_list };
+    return ret;
+  }
 }
