1af2cc3836835dea99d20cbad5c6900cf7c8bd3b
PH5555 <lmkn5342@gmail.com>
2023-07-05 15:12:18 +0900

    feat: config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,16 +7,19 @@ import { BoardImage } from 'src/board/board_image.entity';
 import { BoardTable } from 'src/board/board_table.entity';
 import { CommentTable } from 'src/comment/comment_table.entity';
 import { StudyTable } from 'src/study/study_table.entity';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  port: 5432,
-  database: 'board-app',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  synchronize: true,
-  keepConnectionAlive: true,
+  url: process.env.RDS_URL,
+  synchronize: dbConfig.synchronize,
+  // extra: {
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
+  // },
   entities: [
     UserTable,
     Attendance,
