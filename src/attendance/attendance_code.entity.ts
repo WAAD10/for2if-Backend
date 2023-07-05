c7c660be31359a1100400a430c3153bfdf40b09d
PH5555 <lmkn5342@gmail.com>
2023-07-05 16:33:39 +0900

    Update attendance_code.entity.ts

--- src/attendance/attendance_code.entity.ts
+++ src/attendance/attendance_code.entity.ts
@@ -1,29 +1,38 @@
-import { StudyTable } from "src/study/study_table.entity";
-import { BaseEntity, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Attendance } from "./attendance.entity";
+import { StudyTable } from 'src/study/study_table.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Attendance } from './attendance.entity';
 
 @Entity()
-export class AttendanceCode extends BaseEntity
-{
-    @PrimaryGeneratedColumn()
-    attendance_code_id : number;
+export class AttendanceCode extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  attendance_code_id: number;
 
-    @Column({type : 'integer'})
-    code : number;
+  @Column({ type: 'integer' })
+  code: number;
 
-    @Column({type : 'character varying'})
-    expire_time : string;
+  @Column({ type: 'character varying' })
+  expire_time: string;
 
-    @Column({type : 'character varying'})
-    release_time : string;
+  @Column({ type: 'character varying' })
+  release_time: string;
 
-    @ManyToOne(type=>StudyTable, study_table => study_table.attendance_codes, {eager : true})
-    study : StudyTable;
+  @ManyToOne(
+    (type) => StudyTable,
+    (study_table) => study_table.attendance_codes,
+  )
+  study: StudyTable;
 
-    //////////////////////////////////
+  //////////////////////////////////
 
-    @OneToMany((type) => Attendance, (attendance) => attendance.attendance_code, {
-        eager: true,
-      })
-      attendances: Attendance[];
+  @OneToMany((type) => Attendance, (attendance) => attendance.attendance_code, {
+    eager: true,
+  })
+  attendances: Attendance[];
 }
